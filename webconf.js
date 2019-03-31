30b23f83ee3a3494ea8ec66d02bfc5f04147723d
杰安 陆 <lujiean@outlook.com>
2019-03-31 22:18:01 +0800

    31032019

--- webconf.js
+++ webconf.js
@@ -1,28 +1,72 @@
 angular.module('demo', [])
 .controller('ExampleCtrl', function($scope) {
-    $scope.what = "kk";
-    $scope.people = [
+    // $scope.what = "kk";
+    // $scope.people = [
+    //     {name:'Jon', age: 30, title: 'Developer'},
+    //     {name:'Mike', age: 37, title: 'Manager'},
+    //     {name:'Allen', age: 50, title: 'CEO'},
+    //     {name:'', age: '', title: ''}
+    //     ];
+    $scope.model=[
         {name:'Jon', age: 30, title: 'Developer'},
         {name:'Mike', age: 37, title: 'Manager'},
         {name:'Allen', age: 50, title: 'CEO'},
         {name:'', age: '', title: ''}
-        ];
+    ];
+    // $scope.name=['Jon','Mike','Allen',null];
+    // $scope.age=[30, 37, 50, null];
+    // $scope.title=['Developer','Manager','CEO'];
     
-      $scope.addPerson = function(){
-        var person = {
-            name: $scope.name,
-            age: $scope.age,
-            title: $scope.title,
-        };
+    $scope.addPerson = function(){
+      // if (isUndefinedOrNull($scope.name[index]))
+      //   return;
+      // var person = {
+      //     name: $scope.name,
+      //     age: $scope.age,
+      //     title: $scope.title,
+      // };
+  
+      // $scope.people.pop();
+      // $scope.people.push(person);
+      // $scope.people.push({name:'', age: '', title: ''});
+      // $scope.name.splice(1, 0, null);
+      // $scope.age.splice(1, 0, null);
+      // $scope.title.splice(1, 0, null);
+      // $scope.model.splice($scope.model.length - 1, 0, null);
+      $scope.model.push({name:'', age: '', title: ''});
+      // $scope.age.splice($scope.age.length - 1, 0, null);
+      // $scope.title.splice($scope.title.length - 1, 0, null);
+      
+    }; 
     
-        $scope.people.push(person);
-      }; 
-    
-      $scope.removePerson = function(index){
-        $scope.people.splice(index, 1);
-      };
+    $scope.removePerson = function(index){
+      $scope.model.splice(index, 1);
+      // $scope.age.splice(index, 1);
+      // $scope.title.splice(index, 1);
+    };
 
-      $scope.checkHide = function () {
-        return true;
-      }
+    // $scope.updatePerson = function(index){
+    //   var person = {
+    //     name: $scope.name,
+    //     age: $scope.age,
+    //     title: $scope.title,
+    // };
+      // $scope.people[index] = person;
+    // };
+
+    // $scope.checkHide = function (index) {
+      // if(!isUndefinedOrNull($scope.people[index].name))
+    //   if(!isUndefinedOrNull($scope.name[index]))
+    //     return true;
+    //   else
+    //     return false;
+    // }
 })
+
+// function isUndefinedOrNull(a){
+//   if (!a || typeof(a) == 'undefined' ){
+//       return true;
+//   } else {
+//       return false;
+//   }
+// };
